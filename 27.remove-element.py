01e0e649fa77fee6cfa50413e3012e5f6af5399d
Nini Cui <nini.ccui@gmail.com>
2025-10-31 12:45:05 +1100

    remove element

---
+++ 27.remove-element.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        nums_len = len(nums)
+        left = right = 0
+
+        while right < nums_len:
+            if nums[right] == val:
+                right += 1
+            else:
+                nums[left] = nums[right]
+                left += 1
+                right += 1
+
+        return len(nums[:left])
+        
+# @lc code=end
+if __name__ == "__main__":
+    s = Solution()
+    res = s.removeElement([3, 2, 2, 3], 3)
+    print(res)
