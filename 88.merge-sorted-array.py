2d7e6ee7d9791774ad609aace83b2eaefaceb8c0
Nini Cui <nini.ccui@gmail.com>
2026-03-15 16:18:47 +1100

    add merge sorted array

--- 88.merge-sorted-array.py
+++ 88.merge-sorted-array.py
@@ -7,24 +7,20 @@ from typing import List
 # @lc code=start
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
         while m != 0 and n != 0:
-            diff = nums2[n-1] - nums1[m-1]
-            if diff >= 0:
-                nums1[m+n-1] = nums2[n-1]
-                n -= 1
+            if nums1[m-1] > nums2[n-1]:
+               nums1[m+n-1] = nums1[m-1]
+               m -= 1
             else:
-                nums1[m+n-1] = nums1[m-1] 
-                m -= 1
+                nums1[m+n-1] = nums2[n-1]
+                n-=1
 
         if n != 0:
-            nums1[0:n] = nums2[0:n]
+            nums1[0:n] = nums2[:n]
 
         return nums1
 # @lc code=end
 if __name__ == "__main__":
     s = Solution()
-    res = s.merge(nums1 = [2, 0], m = 1, nums2 = [1], n = 1)
+    res = s.merge(nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3)
     print(res)
