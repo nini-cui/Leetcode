9f4d487bcf0dfd4c411ced9c07973b76b0629e68
Nini Cui <nini.ccui@gmail.com>
2026-04-06 22:25:11 +1000

    add roman to integer

---
+++ 13.roman-to-integer.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=13 lang=python3
+#
+# [13] Roman to Integer
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        mapping = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        s = s.replace('IV', 'IIII').replace('IX', 'VIIII')
+        s = s.replace('XL', 'XXXX').replace('XC', 'LXXXX')
+        s = s.replace('CD', 'CCCC').replace('CM', 'DCCCC')
+        sum = 0
+
+        for i in s:
+            sum += mapping[i]
+
+        return sum
+        
+# @lc code=end
+if __name__ == "__main__":
+    s = Solution()
+    print(s.romanToInt('MCMXCIV'))
