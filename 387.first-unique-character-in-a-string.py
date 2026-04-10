c1c7e94230e76c625ac4cb64662a98036876a946
Nini Cui <nini.ccui@gmail.com>
2026-04-10 23:13:37 +1000

    add first unique char

---
+++ 387.first-unique-character-in-a-string.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=387 lang=python3
+#
+# [387] First Unique Character in a String
+#
+from collections import defaultdict
+# @lc code=start
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        res_dict = defaultdict(int)
+
+        for i in s:
+            res_dict[i] += 1
+
+        for k, v in res_dict.items():
+            if v == 1:
+                return s.find(k)
+        
+        return -1
+# @lc code=end
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.firstUniqChar("aabb"))
